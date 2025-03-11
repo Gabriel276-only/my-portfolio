c138dab5ffee200d62589d902104c40aa1463c96
Gabriel276-only <gabrielmagaborges@gmail.com>
2025-03-11 15:58:42 -0300

    tentando ajustar

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: './',
-  build: {
-    rollupOptions: {
-      external: ['aos', 'react-icons/fa', 'framer-motion'], // Add 'framer-motion' here
-    },
-  },
+
+  
 });
